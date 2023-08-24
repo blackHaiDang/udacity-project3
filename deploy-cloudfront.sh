45b1fd58e00bdcc84a2a7e107eae49867b8c3df8
blackHaiDang <nguyenhaidang.simple@gmail.com>
2023-08-24 20:06:56 +0700

    first steps

---
+++ deploy-cloudfront.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/sh
+
+cd .circleci/files
+aws cloudformation deploy \
+            --template-file cloudfront.yml \
+            --stack-name InitialStack\
+            --parameter-overrides WorkflowID=udapeople-haidangproject3
